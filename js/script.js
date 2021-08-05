16251f8babe33d2ca2696c941af9436a9bc869f0
Teslmik <teslmik@gmail.com>
2021-08-05 13:43:12 +0300

    js

--- js/script.js
+++ js/script.js
@@ -12,21 +12,116 @@ window.addEventListener("scroll", function () {
 });
   //-------------------------------------------------------Menu Scrolled End--------------------
 
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+  //----------------------Slider Main---------------------
+if (document.querySelector('.slider-swiper-container')){
+  new Swiper('.slider-swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider .swiper-button-next',
+      prevEl: '.slider .swiper-button-prev',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    slidesPerView: 1,
 
-});
+    autoplay: {
+      delay: 5000,
+      stopOnLastSlide: false,
+      disableOnInteraction: false,
+    }
+
+  });
+}
+  
+  //----------------------Slider Main End---------------------
+
+
+  //----------------------Slider Feedback---------------------
+if (document.querySelector('.swiper-container')) {
+
+  new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.feedback .swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.feedback .swiper-button-next',
+      prevEl: '.feedback .swiper-button-prev',
+    },
+
+    spaceBetween: 50,
+
+    autoplay: {
+      delay: 5000,
+      stopOnLastSlide: false,
+      disableOnInteraction: false,
+    }
+
+  });
+}
+  //----------------------Slider Feedback End---------------------
+
+  //-------------------------------IBG IMG--------------------
+function ibg(){
+
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+      ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+    }
+  }
+}
+
+ibg();
+
+// -----------------------IBG END-----------------------
+
+// --------------------Scroll smooth--------------------
+const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+      
+      if (iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+      }
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    }
+  }
+}
+
+// ---------------click burger----------------------
+const iconMenu = document.querySelector('.menu-icon');
+const menuBody = document.querySelector('.menu');
+if (iconMenu) {
+  iconMenu.addEventListener('click', function (e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
